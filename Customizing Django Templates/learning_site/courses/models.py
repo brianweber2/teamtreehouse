d2dd0ae14283a33af131a5456cef7b12ba440ff9
Brian Weber <brianweber2@gmail.com>
2016-09-12 21:29:06 -0700

    Added Customizing Django Templates.

---
+++ Customizing Django Templates/learning_site/courses/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Course(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
+
+
+class Step(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    content = models.TextField(blank=True, default='')
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(Course)
+    
+    class Meta:
+        ordering = ['order',]
+    
+    def __str__(self):
+        return self.title
